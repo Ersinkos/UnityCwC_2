4a4a9d0be10a9dffab373fd15f412bf7f60827e6
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-08-30 17:20:44 +0300

    Collisions done

--- UnityCwC_2/Assets/Scripts/SpawnManager.cs
+++ UnityCwC_2/Assets/Scripts/SpawnManager.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            animalIndex = Random.Range(0, animalPrefabs.Length);
-            Debug.Log(animalIndex);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Debug.Log(animalIndex);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
